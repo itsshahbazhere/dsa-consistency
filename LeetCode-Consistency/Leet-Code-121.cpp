9e409db4419d01247f61e413ba20c1c7938cc2e5
Md Shahbaz Alam <sa0418600@gmail.com>
2024-10-28 15:55:14 +0530

    Solving leetcode question 121

--- LeetCode-Consistency/Leet-Code-121.cpp
+++ LeetCode-Consistency/Leet-Code-121.cpp
@@ -1,21 +1,50 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+//approach -2  beats 100%
+// TIME COMPLEXITY = O(n)
+// SPACE COMPLEXITY = O(1)
+
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        
+        int buy = prices[0];
+        int maxProfit = 0;
+
+        for(int i=1; i<prices.size(); i++){
+
+            if(prices[i] < buy){
+                buy = prices[i];
+            }
+            else if(prices[i] - buy > maxProfit){
+                maxProfit = prices[i] - buy;
+            }
+        }
+
+        return maxProfit;
+    }
+};
 
 
+
+
+//approach -1 
+// TIME COMPLEXITY = O(n^2)
+// SPACE COMPLEXITY = O(1)
+
+ class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
 
         int n = prices.size();
-        int maxProfit = INT_MIN;
+        int maxProfit = 0;
         for(int i=0; i<n; i++){
             for(int j=i+1;j<n;j++){
                 int sellPrice=prices[j] - prices[i];
                 maxProfit = max(maxProfit,sellPrice);
             }
         }
-
-        if(maxProfit < 0) return 0;
         
         return maxProfit;
         
