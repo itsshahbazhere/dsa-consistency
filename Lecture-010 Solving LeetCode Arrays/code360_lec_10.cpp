2d604152b764438514bd5d494284fba6a204ab89
itsshahbazhere <sa0418600@gmail.com>
2025-06-23 19:46:40 +0530

    changing somting after long time

--- Lecture-010 Solving LeetCode Arrays/code360_lec_10.cpp
+++ Lecture-010 Solving LeetCode Arrays/code360_lec_10.cpp
@@ -1,5 +1,21 @@
 //  Duplicate In Array link:https://www.naukri.com/code360/problems/duplicate-in-array_893397?source=youtube&campaign=love_babbar_codestudio1&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio1&leftPanelTabValue=PROBLEM
 
+//we can do in a signle for loop as well
+int findDuplicate(vector<int> &arr) 
+{
+    int size = arr.size();
+    int ans = 0;
+    for(int i=0; i<size; i++){
+        ans^=arr[i];
+        if(i){
+            ans^=i;
+        }
+    }
+
+    return ans;
+	
+}
+
 
 int findDuplicate(vector<int> &arr) 
 {
@@ -18,3 +34,4 @@ int findDuplicate(vector<int> &arr)
     return ans;
 	
 }
+
