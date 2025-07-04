7138f88b9893f170743794b14f742c12ddcc0fb8
itsshahbazhere <sa0418600@gmail.com>
2025-07-05 01:05:27 +0530

    itsshahbazhere file deleted

--- Lecture-022 Strings/Leet-Code-443.cpp
+++ Lecture-022 Strings/Leet-Code-443.cpp
@@ -4,38 +4,27 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i = 0; // Points to the current position in the input array
-        int n = chars.size(); // Size of the input array
-        int ansIndex = 0; // Index to place the compressed characters
-
-        while (i < n) {
-            int j = i + 1; // Points to the next character
-            // Loop to find the end of the current group of the same characters
-            while (j < n && chars[i] == chars[j]) {
+        
+        int i=0;
+        int n = chars.size();
+        int ansIndex = 0;
+        while(i<n){
+            int j=i+1;
+            while(j<n && chars[i] == chars[j]){
                 j++;
             }
-
-            // At this point, either we have reached the end of the array,
-            // or found a different character.
-
-            // Store the current character
             chars[ansIndex++] = chars[i];
-
-            // Calculate the count of the current character
-            int count = j - i;
-            if (count > 1) {
-                // Convert the count to a string and store each digit
+            int count = j-i;
+            if(count>1){
                 string cnt = to_string(count);
-                for (char ch : cnt) {
-                    chars[ansIndex++] = ch;
+                for(int k=0; k<cnt.length(); k++){
+                    chars[ansIndex++] = cnt[k];
                 }
             }
 
-            // Move to the next group of characters
-            i = j;
+            i=j;
         }
 
         return ansIndex;
     }
 };
-
