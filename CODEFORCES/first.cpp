1ac058f034626ce2ab9f2795952156110352e4ac
itsshahbazhere <sa0418600@gmail.com>
2025-08-16 00:21:11 +0530

    first time solve first problem in codeforces

---
+++ CODEFORCES/first.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,a,b,c;
+    cin>>n;
+    while(n--){
+        cin>>a>>b>>c;
+        if(a+b==c || b+c == a || a+c == b){
+            cout<<"yes\n";
+        }
+        else{
+            cout<<"no\n";
+        }
+
+    }
+    return 0;
+}
